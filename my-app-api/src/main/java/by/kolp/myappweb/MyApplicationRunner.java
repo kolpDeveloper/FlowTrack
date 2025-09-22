087f5c6dcc228e5599c11bd1a616c1a566cff9d7
kolp <hasta21q@gmail.com>
2025-09-22 22:12:54 +0500

    minor update

--- my-app-api/src/main/java/by/kolp/myappweb/MyApplicationRunner.java
+++ my-app-api/src/main/java/by/kolp/myappweb/MyApplicationRunner.java
@@ -2,10 +2,18 @@ package by.kolp.myappweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"by.kolp.myappproducer, by.kolp.core"})
+@SpringBootApplication(scanBasePackages = {
+        "by.kolp.myappweb",
+        "by.kolp.myappcore",
+        "by.kolp.myappproducer"
+})
+@EntityScan("by.kolp.myappcore.model.entity")
+@EnableJpaRepositories("by.kolp.myappcore.repository")
 public class MyApplicationRunner {
     public static void main(String[] args) {
-        SpringApplication.run(MyApplicationRunner.class);
+        SpringApplication.run(MyApplicationRunner.class, args);
     }
 }
