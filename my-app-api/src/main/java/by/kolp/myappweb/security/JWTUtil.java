9d801f848bda69d83654a776323625b9ad23e457
kolp <hasta21q@gmail.com>
2025-09-27 22:48:29 +0500

    refactor: remove hardcoded values

--- my-app-api/src/main/java/by/kolp/myappweb/security/JWTUtil.java
+++ my-app-api/src/main/java/by/kolp/myappweb/security/JWTUtil.java
@@ -8,30 +8,33 @@ import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
 import java.time.Instant;
-import java.time.ZonedDateTime;
 import java.util.Date;
 
 
 @Component
 public class JWTUtil {
 
-    @Value("${jwt_secret}")
+    @Value("${jwt.secret}")
     private String secret;
 
+    @Value("${jwt.expiration:PT1H}")
+    private Duration tokenExpiration;
+
     public String generateToken(String username) {
-        Date expiredDate = Date.from(ZonedDateTime.now().plusMinutes(60).toInstant());
+
+        Date expiredDate = Date.from(Instant.now().plus(tokenExpiration));
+
         return JWT.create()
                 .withSubject(username)
                 .withIssuedAt(Instant.now())
-                .withIssuer("kolp")
                 .withExpiresAt(expiredDate)
                 .sign(Algorithm.HMAC256(secret));
     }
 
     public String validateToken(String token) throws JWTVerificationException {
         JWTVerifier verifier = JWT.require(Algorithm.HMAC256(secret))
-                .withIssuer("kolp")
                 .build();
 
         DecodedJWT decode = verifier.verify(token);
