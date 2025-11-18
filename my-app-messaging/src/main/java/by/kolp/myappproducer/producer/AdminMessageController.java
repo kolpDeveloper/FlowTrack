b744c4ba0588b25eb08737cd110d8fa1e248e417
kolp <hasta21q@gmail.com>
2025-11-18 22:31:42 +0500

    reformat: minor changes

--- my-app-messaging/src/main/java/by/kolp/myappproducer/producer/AdminMessageController.java
+++ my-app-messaging/src/main/java/by/kolp/myappproducer/producer/AdminMessageController.java
@@ -1,12 +1,18 @@
 package by.kolp.myappproducer.producer;
 
-import by.kolp.myappproducer.dto.SubjectMessageDTO;
+import by.kolp.myappproducer.dto.AdminEmailRequest;
 import by.kolp.myappproducer.service.MailSenderService;
+import by.kolp.myappproducer.service.MessageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -14,9 +20,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class AdminMessageController {
 
     private final MailSenderService mailSenderService;
+    private final MessageService messageService;
 
     @PostMapping("/admin/send/bulk")
-    public void sendHtml(@RequestBody SubjectMessageDTO request){
-        mailSenderService.sendToQueue(request);
+    public ResponseEntity<String> sendBulk(@RequestBody AdminEmailRequest request)
+    {
+        Page<String> emails = messageService.findAllEmails(Pageable.unpaged());
+
+        mailSenderService.send(emails, request.getSubject(), request.getMessage());
+        log.info("Messages successfully sent!");
+        return ResponseEntity.status(200).build();
     }
+
+
 }
