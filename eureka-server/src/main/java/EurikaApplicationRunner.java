6f7cb398cf6e50b71213b7fc6402db771af8197b
kolp <hasta21q@gmail.com>
2025-11-25 17:52:41 +0500

    feat: add eureka-server module

---
+++ eureka-server/src/main/java/EurikaApplicationRunner.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurikaApplicationRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(EurikaApplicationRunner.class, args);
+    }
+}
