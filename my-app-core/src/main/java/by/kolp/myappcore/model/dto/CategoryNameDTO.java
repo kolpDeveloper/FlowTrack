535e26cdcc402a428e5294cdf5efe8d164eb622e
kolp <hasta21q@gmail.com>
2025-11-25 17:48:42 +0500

    feat: add finance-service module

--- my-app-core/src/main/java/by/kolp/myappcore/model/dto/CategoryNameDTO.java
+++
@@ -1,6 +0,0 @@
-package by.kolp.myappcore.model.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record CategoryNameDTO(@NotBlank String name) {
-}
--- my-app-core/src/main/java/by/kolp/myappcore/model/dto/NumericDataEntryDTO.java
+++
@@ -1,8 +0,0 @@
-package by.kolp.myappcore.model.dto;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import java.time.Instant;
-
-public record NumericDataEntryDTO(String key, Integer value, @JsonProperty("created_at") Instant createdAt, CategoryNameDTO category) {
-}
