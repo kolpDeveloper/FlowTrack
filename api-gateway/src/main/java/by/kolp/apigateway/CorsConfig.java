28cbd29bfb51a5cc6b684b6a3d211379b29b98e4
rishq <hasta21q@gmail.com>
2026-08-09 16:52:07 +0500

    feat: cors config

---
+++ api-gateway/src/main/java/by/kolp/apigateway/CorsConfig.java
@@ -0,0 +1,49 @@
+package by.kolp.apigateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:3000"
+        ));
+
+        configuration.setAllowedMethods(List.of(
+                "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"
+        ));
+
+        configuration.setAllowedHeaders(List.of(
+                "Authorization",
+                "Content-Type",
+                "X-USER-ID",
+                "X-USER-NAME",
+                "X-USER-AUTHORITIES"
+        ));
+
+        configuration.setExposedHeaders(List.of(
+                "Authorization",
+                "X-USER-ID",
+                "X-USER-NAME",
+                "X-USER-AUTHORITIES"
+        ));
+
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return new CorsWebFilter(source);
+    }
+}
